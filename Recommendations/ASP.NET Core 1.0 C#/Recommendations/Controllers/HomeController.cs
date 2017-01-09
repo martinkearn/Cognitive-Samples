e880d92baa171570cb0115aa6816f64477068f64
Martin Kearn <martin.kearn@microsoft.com>
2017-01-09 17:06:32 +0000

    List and Details pages now working

--- Recommendations/ASP.NET Core 1.0 C#/Recommendations/Controllers/HomeController.cs
+++ Recommendations/ASP.NET Core 1.0 C#/Recommendations/Controllers/HomeController.cs
@@ -11,60 +11,31 @@ using System.Text;
 using Recommendations.Models;
 using Recommendations.ViewModels;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Recommendations.Services;
 
 namespace Recommendations.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IHostingEnvironment _environment;
+        private readonly List<Book> _books;
 
         public HomeController(IHostingEnvironment env)
         {
             _environment = env;
+            _books = new BooksService(_environment).GetBooks();
         }
 
         public IActionResult Index()
         {
-            //get store data
-            var books = new List<Book>();
-            var rootPath = _environment.ContentRootPath;
-            var storeFilePath = rootPath + "/bookscatalog.txt";
-            using (var fileStream = new FileStream(storeFilePath, FileMode.Open))
-            {
-                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
-                {
-                    string line;
-                    while ((line = streamReader.ReadLine()) != null)
-                    {
-                        var cells = line.Split(',');
-                        var book = new Book()
-                        {
-                            Id = cells[0],
-                            Title = cells[1],
-                            Type = cells[2],
-                            Author = cells[3].Substring(cells[3].IndexOf('=')+1),
-                            Publisher = cells[4].Substring(cells[4].IndexOf('=') + 1),
-                            Year = cells[5].Substring(cells[5].IndexOf('=') + 1)
-                        };
-                        books.Add(book);
-                    }
-                }
-            }
-
             //render view
-            return View(books);
+            return View(_books);
         }
 
         public IActionResult Book(string id)
         {
-            var book = new Book() {
-                Id = id,
-                Title = string.Empty,
-                Type = string.Empty,
-                Author = string.Empty,
-                Publisher = string.Empty,
-                Year = string.Empty
-            };
+            var book = _books.Where(o => o.Id == id).FirstOrDefault();
+
             return View(book);
         }
 
---
+++ Recommendations/ASP.NET Core 1.0 C#/Recommendations/Services/BooksService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Recommendations.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recommendations.Services
+{
+    public class BooksService
+    {
+        private readonly IHostingEnvironment _environment;
+
+        public BooksService(IHostingEnvironment env)
+        {
+            _environment = env;
+        }
+
+        public List<Book> GetBooks()
+        {
+            var books = new List<Book>();
+            var rootPath = _environment.ContentRootPath;
+            var storeFilePath = rootPath + "/bookscatalog.txt";
+            using (var fileStream = new FileStream(storeFilePath, FileMode.Open))
+            {
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    string line;
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        var cells = line.Split(',');
+                        var book = new Book()
+                        {
+                            Id = cells[0],
+                            Title = cells[1],
+                            Type = cells[2],
+                            Author = cells[3].Substring(cells[3].IndexOf('=') + 1),
+                            Publisher = cells[4].Substring(cells[4].IndexOf('=') + 1),
+                            Year = cells[5].Substring(cells[5].IndexOf('=') + 1)
+                        };
+                        books.Add(book);
+                    }
+                }
+            }
+
+            return books;
+        }
+    }
+}
