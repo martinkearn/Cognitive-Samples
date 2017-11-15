66a76ebd2ae566bd6bdadd6b0fc166fe744aec30
Martin Kearn <martin.kearn@microsoft.com>
2017-11-15 13:52:53 +0000

    App Settings

---
+++ Face/Person Groups and Face Identify/ASP.NET Core 2.0 C#/PersonGroupSample/Models/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonGroupSample.Models
+{
+    public class AppSettings
+    {
+        public string FaceApiKey { get; set; }
+        public string FaceApiEndpoint { get; set; }
+    }
+}
--- Face/Person Groups and Face Identify/ASP.NET Core 2.0 C#/PersonGroupSample/Startup.cs
+++ Face/Person Groups and Face Identify/ASP.NET Core 2.0 C#/PersonGroupSample/Startup.cs
@@ -11,12 +11,17 @@ namespace PersonGroupSample
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfigurationRoot Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
