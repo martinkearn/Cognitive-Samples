f537d9ca47ce74ca32d48ec32aac879e7245027e
Martin Kearn <martin.kearn@microsoft.com>
2017-11-17 12:56:24 +0000

    Person group details

---
+++ Face/Person Groups and Face Identify/ASP.NET Core 2.0 C#/PersonGroupSample/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonGroupSample.Models
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string userData { get; set; }
+    }
+}
